b20af81644cb83e999fcd7114d52310d3b51cf72
dev-asib <mail.asib.com@gmail.com>
2024-10-28 23:40:05 +0600

    Updated

--- lib/resources/constants/assets_paths.dart
+++ lib/resources/constants/assets_paths.dart
@@ -8,7 +8,7 @@ class AssetsPaths{
   static const String _soundsPath = '$_basePath/sounds';
 
   static const String backgroundSvg = "$_imagesPath/background.svg";
-  static const String taskNextXSvg = "$_imagesPath/task_next_x.svg";
+  static const String taskNextXSvg = "$_imagesPath/logo.svg";
 
   static const String emptyTask = "$_lottiesPath/empty_task.json";
 
